05a9f78ee6414419adc475e0ab7004ec7b19b53e
Ryan L <toranlyt@gmail.com>
2019-07-17 20:40:42 -0500

    Implemented working inRange method

--- _.js
+++ _.js
@@ -1,7 +1,26 @@
+const _ = {
+  clamp(num, lower, upper) {
+    let lowerClampedValue = Math.max(num, lower);
+    let clampedValue = Math.min(lowerClampedValue, upper);
+    return clampedValue;
+  },
 
-
-
-
-
+  inRange(number, start, end){
+    if(!end){
+      end = start;
+      start = 0;
+    }
+    if(start > end){
+      let s2 = start;
+      start = end;
+      end = s2;
+    }
+    if(number >= start && number < end){
+      return true;
+    } else{
+      return false;
+    }
+  }
+};
 // Do not write or modify code below this line.
 module.exports = _;
